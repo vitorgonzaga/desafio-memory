8b877c169e48e444a0b1894bb4cc1aedb54cabbf
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-30 18:52:05 -0300

    feat: adiciona migration para popular dados na tabela fabricantes

---
+++ src/main/resources/db/migration/V5__insert-values-into-fabricantes.sql
@@ -0,0 +1,4 @@
+insert into fabricantes (nome, cnpj) values ("fornecedorA", "123.456.789/0001-00");
+insert into fabricantes (nome, cnpj) values ("fornecedorB", "987.654.321/0001-00");
+insert into fabricantes (nome, cnpj) values ("fornecedorC", "111.111.111/0001-00");
+
