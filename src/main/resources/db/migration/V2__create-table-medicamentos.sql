6fd16a246ee4e7aa3972d388a232ac0317957a9e
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-30 16:20:25 -0300

    feat(database): adiciona migration para criacao da tabela medicamentos

---
+++ src/main/resources/db/migration/V2__create-table-medicamentos.sql
@@ -0,0 +1,15 @@
+create table medicamentos(
+
+	id bigint not null auto_increment,
+	registro_anvisa varchar(20) not null unique,
+	nome varchar(100) not null,
+	data_validade datetime not null,
+	telefone varchar(20) not null,
+	preco float not null,
+	quantidade_comprimidos int not null,
+	fabricante_id bigint not null,
+
+	primary key(id),
+	constraint fk_medicamentos_fabricante_id foreign key(fabricante_id) references fabricantes(id)
+
+);
