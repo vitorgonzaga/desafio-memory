2ea5a0a13034da9ce19bb07837132662ae26f50f
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 16:18:12 -0300

    feat(MedicamentoController): injeta o MedicamentoRepository e adiciona o método atualiza (PUT) e o método listar (GET) utilizando-se do recurso de paginacao do Spring e o método default do findAll da interface JpaRepository;

--- src/main/java/com/memory/gerenciador/controller/MedicamentoController.java
+++ src/main/java/com/memory/gerenciador/controller/MedicamentoController.java
@@ -3,12 +3,12 @@ package com.memory.gerenciador.controller;
 import com.memory.gerenciador.domain.medicamento.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -16,6 +16,9 @@ import java.net.URI;
 @RestController
 @RequestMapping("medicamentos")
 public class MedicamentoController {
+
+     @Autowired
+     MedicamentoRepository medicamentoRepository;
 
     @Autowired
     private MedicamentoService medicamentoService;
@@ -28,5 +31,19 @@ public class MedicamentoController {
         URI uri = uriComponentsBuilder.path("/medicamento/{id}").buildAndExpand(dto.id()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedicamento dados) {
+        Medicamento medicamento = medicamentoService.atualizar(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoMedicamento(medicamento));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoMedicamento>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        Page<DadosDetalhamentoMedicamento> page = medicamentoRepository.findAll(paginacao).map(DadosDetalhamentoMedicamento::new);
+        return ResponseEntity.ok(page);
+    }
+
 
 }
