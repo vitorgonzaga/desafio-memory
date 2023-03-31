dd094ef2bdb2bd53838d6d6f4b4e316adb67c624
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 10:00:51 -0300

    feat: adiciona MedicamentoRepository para persistencia da entidade Medicamento

---
+++ src/main/java/com/memory/gerenciador/domain/medicamento/MedicamentoRepository.java
@@ -0,0 +1,6 @@
+package com.memory.gerenciador.domain.medicamento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
+}
