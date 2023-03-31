882a058f240d1789eb362562408171124fd80ab3
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 10:02:02 -0300

    feat: adiciona ReacaoRepository para persistencia da entidade Reacao;

---
+++ src/main/java/com/memory/gerenciador/domain/reacao/ReacaoRepository.java
@@ -0,0 +1,6 @@
+package com.memory.gerenciador.domain.reacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReacaoRepository extends JpaRepository<Reacao, Long> {
+}
