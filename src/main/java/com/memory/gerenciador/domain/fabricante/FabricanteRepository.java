781608bd7a2de2da47be2fef3b5cd07115ca34a2
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 10:01:40 -0300

    feat: adiciona FabricanteRepository para persistencia da entidade Fabricante;

---
+++ src/main/java/com/memory/gerenciador/domain/fabricante/FabricanteRepository.java
@@ -0,0 +1,6 @@
+package com.memory.gerenciador.domain.fabricante;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FabricanteRepository extends JpaRepository<Fabricante, Long> {
+}
