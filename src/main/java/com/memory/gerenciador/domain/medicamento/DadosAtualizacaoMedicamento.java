e58d96ed4c07932484fd66f3ac4efa6eac04c2f4
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 16:12:48 -0300

    refactor(DadosAtualizacaoMedicamento): adiciona algumas annotations para validação dos formatos enviados no Json;

--- src/main/java/com/memory/gerenciador/domain/medicamento/DadosAtualizacaoMedicamento.java
+++ src/main/java/com/memory/gerenciador/domain/medicamento/DadosAtualizacaoMedicamento.java
@@ -1,8 +1,7 @@
 package com.memory.gerenciador.domain.medicamento;
 
-import com.memory.gerenciador.domain.fabricante.DadosFabricante;
-import com.memory.gerenciador.domain.reacao.Reacao;
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,10 +12,17 @@ public record DadosAtualizacaoMedicamento(
         @NotNull
         Long id,
         String nome,
+        @Pattern(regexp = "\\d{1}\\.\\d{4}\\.\\d{4}\\.\\d{3}\\-\\d{1}")
         String registroAnvisa,
+        @Future
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataValidade,
+        @Pattern(regexp = "\\(\\d{2}\\)\\d{4}\\-\\d{4}")
         String telefone,
+        @DecimalMin(value = "0.0", inclusive = false)
+        @Digits(integer = 8, fraction = 2)
         BigDecimal preco,
+        @Min(value = 1)
         Integer quantidadeComprimidos,
         Long fabricanteId,
         List<Long> reacoes
