59609516ce2b022687c953730bdeb4efe9c88f38
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-31 16:15:14 -0300

    feat(Medicamento): adiciona o método atualizarInformações;

--- src/main/java/com/memory/gerenciador/domain/medicamento/Medicamento.java
+++ src/main/java/com/memory/gerenciador/domain/medicamento/Medicamento.java
@@ -49,4 +49,31 @@ public class Medicamento {
     )
     private List<Reacao> reacoes;
 
+    public void atualizarInformacoes(DadosAtualizacaoMedicamento dados, Fabricante fabricante, List<Reacao> reacoes) {
+        if(dados.registroAnvisa() != null) {
+            this.registroAnvisa = dados.registroAnvisa();
+        }
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.dataValidade() != null) {
+            this.dataValidade = dados.dataValidade();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.preco() != null) {
+            this.preco = dados.preco();
+        }
+        if(dados.quantidadeComprimidos() != null) {
+            this.quantidadeComprimidos = dados.quantidadeComprimidos();
+        }
+        if(fabricante != null) {
+            this.fabricante = fabricante;
+        }
+        if(reacoes != null) {
+            this.reacoes = reacoes;
+        }
+
+    }
 }
