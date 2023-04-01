668e8a985872778b2dd6d2eecdca714b3bdd4ec9
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-01 08:56:07 -0300

    feat(V8__insert-values-into-medicamentos.sql): popula o banco com alguns medicamentos para facilitar alguns testes manuais;

---
+++ src/main/resources/db/migration/V8__insert-values-into-medicamentos.sql
@@ -0,0 +1,52 @@
+insert into medicamentos (registro_anvisa, nome, data_validade, telefone, preco, quantidade_comprimidos, fabricante_id)
+values (
+    "0.0000.0000.001-1",
+    "Targifor",
+    "2024-04-01",
+    "(11)2581-2980",
+    39.90,
+    20,
+    2
+);
+insert into medicamentos (registro_anvisa, nome, data_validade, telefone, preco, quantidade_comprimidos, fabricante_id)
+values (
+    "0.0000.0000.002-2",
+    "Novalgina",
+    "2024-06-01",
+    "(11)2581-3333",
+    49.90,
+    30,
+    2
+);
+insert into medicamentos (registro_anvisa, nome, data_validade, telefone, preco, quantidade_comprimidos, fabricante_id)
+values (
+    "0.0000.0000.003-3",
+    "Alegra",
+    "2024-09-30",
+    "(11)2590-9999",
+    66.90,
+    24,
+    1
+);
+insert into medicamentos (registro_anvisa, nome, data_validade, telefone, preco, quantidade_comprimidos, fabricante_id)
+values (
+    "0.0000.0000.004-4",
+    "Ibuprofeno",
+    "2024-04-30",
+    "(11)2241-7777",
+    8.90,
+    16,
+    1
+);
+insert into medicamentos (registro_anvisa, nome, data_validade, telefone, preco, quantidade_comprimidos, fabricante_id)
+values (
+    "0.0000.0000.005-5",
+    "Paracetamol",
+    "2024-07-31",
+    "(11)2281-8122",
+    16.90,
+    16,
+    3
+);
+
+
