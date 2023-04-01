40ea83bfefd715b5d1073c8cf3df699185e1e962
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-01 08:54:26 -0300

    refactor(DadosDetalhamentoMedico): modifica o formato de data e o formato do preco que retornam no json de detalhamento do medicamento;

--- src/main/java/com/memory/gerenciador/domain/medicamento/DadosDetalhamentoMedicamento.java
+++ src/main/java/com/memory/gerenciador/domain/medicamento/DadosDetalhamentoMedicamento.java
@@ -1,8 +1,10 @@
 package com.memory.gerenciador.domain.medicamento;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.memory.gerenciador.domain.fabricante.DadosFabricante;
-import com.memory.gerenciador.domain.fabricante.Fabricante;
 import com.memory.gerenciador.domain.reacao.Reacao;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,8 +14,11 @@ public record DadosDetalhamentoMedicamento(
         Long id,
         String nome,
         String registroAnvisa,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataValidade,
         String telefone,
+        @DecimalMin(value = "0.0", inclusive = false)
+        @Digits(integer = 8, fraction = 2)
         BigDecimal preco,
         Integer quantidadeComprimidos,
         DadosFabricante fabricante,
