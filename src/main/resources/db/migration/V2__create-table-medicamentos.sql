ce729a21787ebbb4ab5d6d0ab20ebbe351ee41d0
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-01 08:55:15 -0300

    refactor(V2__create-table-medicamentos.sql): altera o tipo datatime para apenas date;

--- src/main/resources/db/migration/V2__create-table-medicamentos.sql
+++ src/main/resources/db/migration/V2__create-table-medicamentos.sql
@@ -3,7 +3,7 @@ create table medicamentos(
 	id bigint not null auto_increment,
 	registro_anvisa varchar(20) not null unique,
 	nome varchar(100) not null,
-	data_validade datetime not null,
+	data_validade date not null,
 	telefone varchar(20) not null,
 	preco float not null,
 	quantidade_comprimidos int not null,
