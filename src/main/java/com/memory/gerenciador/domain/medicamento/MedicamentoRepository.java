1687a20aba71ed68d2b04f4d14b9c99045448fbf
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-04-01 08:06:53 -0300

    feat(MedicamentoRepository): adiciona o método findByNomeLike;

--- src/main/java/com/memory/gerenciador/domain/medicamento/MedicamentoRepository.java
+++ src/main/java/com/memory/gerenciador/domain/medicamento/MedicamentoRepository.java
@@ -2,5 +2,9 @@ package com.memory.gerenciador.domain.medicamento;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {
+
+    List<Medicamento> findByNomeLike(String nome);
 }
